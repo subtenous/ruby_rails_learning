46455e14959af04696d04724274265a8f1720e2e
Marcus Cameron <marcuscameron225@gmail.com>
2026-07-15 17:51:26 +0100

    Add post and user models + validations and associations

--- rails_projects/micro_reddit/app/models/user.rb
+++ rails_projects/micro_reddit/app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            length: { minimum: 3, maximum: 20 },
+            allow_blank: true
+  
+  validates :email,
+            presence: true,
+            uniqueness: true
+
+  validates :password,
+            presence: true,
+            length: { minimum: 6 },
+            allow_blank: true
 end
--- rails_projects/micro_reddit/db/schema.rb
+++ rails_projects/micro_reddit/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_15_161621) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_15_163139) do
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
@@ -18,4 +27,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_15_161621) do
     t.datetime "updated_at", null: false
     t.string "username"
   end
+
+  add_foreign_key "posts", "users"
 end
