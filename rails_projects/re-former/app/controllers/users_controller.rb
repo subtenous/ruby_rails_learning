bf29a856998dbb6ec8ae98b0435e14ebb55ed0f7
Marcus Cameron <marcuscameron225@gmail.com>
2026-07-18 11:22:26 +0100

    Add railsy form for creation, and editing form

--- rails_projects/re-former/app/controllers/users_controller.rb
+++ rails_projects/re-former/app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
-
-  def new  
+  def new
+    @user = User.new
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
@@ -13,10 +12,24 @@ class UsersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-end
 
-private
+  def edit
+    @user = User.find(params[:id])
+  end
 
-def user_params
-  params.expect(user: [ :username, :email, :password ])
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: [:username, :email, :password])
+  end
 end
